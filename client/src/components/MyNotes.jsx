7f7def2cf94c7aa476f0e40458ea454bb0dabf92
VishalKumar1902 <vishalkumarnbd19@gmail.com>
2025-01-12 20:03:28 +0530

    Update MyNotes.jsx

--- client/src/components/MyNotes.jsx
+++ client/src/components/MyNotes.jsx
@@ -5,13 +5,14 @@ import { useAuth } from "../context/AuthContext";
 const MyNotes = () => {
   const [notes, setNotes] = useState([]);
   const { user, isAuthenticated } = useAuth();
+  const [loading, setLoading] = useState(true);
 
   const fetchNotes = async () => {
     // we also need to get the token
     const token = localStorage.getItem("token");
 
     try {
-      const response = await fetch("https://notesapp-qdlm.onrender.com/api/notes", {
+      const response = await fetch("http://localhost:5000/api/notes", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -30,6 +31,9 @@ const MyNotes = () => {
       setNotes(data);
     } catch (error) {
       console.log("Error fetching notes: ", error);
+    } finally {
+      // Set loading to false after fetch completes
+      setLoading(false);
     }
   };
 
@@ -46,7 +50,7 @@ const MyNotes = () => {
     const token = localStorage.getItem("token");
     try {
       const response = await fetch(
-        `https://notesapp-qdlm.onrender.com/api/notes/${noteId}`,
+        `http://localhost:5000/api/notes/${noteId}`,
         {
           method: "DELETE",
           headers: {
@@ -74,17 +78,24 @@ const MyNotes = () => {
       <h3 className="text-3xl font-bold mb-4 mt-[70px] text-center">
         Hi! {user ? user.name : "Guest"}, here are your notes
       </h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
-        {notes.map((note) => (
-          <NotesCard
-            key={note._id}
-            id={note._id}
-            title={note.title}
-            description={note.description}
-            deleteNote={handleDelete} // Pass the delete function
-          />
-        ))}
-      </div>
+      {/* Display a loading message or spinner while loading */}
+      {loading ? (
+        <div className=" flex justify-center items-center p-6">
+          <div className="  rounded-full h-16 w-16 border-t-4 border-red-600 animate-spin"></div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+          {notes.map((note) => (
+            <NotesCard
+              key={note._id}
+              id={note._id}
+              title={note.title}
+              description={note.description}
+              deleteNote={handleDelete} // Pass the delete function
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
